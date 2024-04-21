ed8bd685ab4ab05e69045a6335c990bc779d8edd
rahman.muraman <rahman.muraman@gmail.com>
2024-04-21 14:18:08 +0700

    refactor: mengubah router ke v6

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, createHashRouter, RouterProvider } from "react-router-dom";
 import {
   Home,
   Dashboard,
@@ -12,22 +12,41 @@ import {
 } from "./page/index";
 import "./App.css";
 
+
+const routes = [
+  { path :"/", element : <Home/>},
+  { path :"/home", element : <Home/>},
+  { path:"/dashboard", element:<Dashboard />},
+  { path:"/produk", element:<Product /> },
+  { path:"/produk/tambah", element:<AddProduct /> },
+  { path:"/produk/edit/:id", element:<EditProduct /> },
+  { path:"/transaksi", element:<Transaksi /> },
+  { path:"/transaksi/rincian/:kodeNota", element:<Rincian /> },
+  { path:"*", element:<NotFound /> },
+]
+
+const router = createHashRouter(routes);
+
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route index element={<Home />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/produk" element={<Product />} />
-        <Route path="/produk/tambah" element={<AddProduct />} />
-        <Route path="/produk/edit/:id" element={<EditProduct />} />
-        <Route path="/transaksi" element={<Transaksi />} />
-        <Route path="/transaksi/rincian/:kodeNota" element={<Rincian />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
-  );
+
+  return <RouterProvider router={router}/>
+
+  // return (
+  //   <BrowserRouter>
+  //     <Routes>
+  //       <Route index element={<Home />} />
+  //       <Route path="/home" element={<Home />} />
+  //       <Route path="/dashboard" element={<Dashboard />} />
+  //       <Route path="/produk" element={<Product />} />
+  //       <Route path="/produk/tambah" element={<AddProduct />} />
+  //       <Route path="/produk/edit/:id" element={<EditProduct />} />
+  //       <Route path="/transaksi" element={<Transaksi />} />
+  //       <Route path="/transaksi/rincian/:kodeNota" element={<Rincian />} />
+  //       <Route path="*" element={<NotFound />} />
+  //     </Routes>
+  //   </BrowserRouter>
+  // );
+
 }
 
 export default App;
